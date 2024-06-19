ae97744bf96516374148d46277d6f5b6f59301c4
Lautaro <orbesle@gmail.com>
2024-06-18 21:35:46 -0300

    add logs

--- instantneo/core.py
+++ instantneo/core.py
@@ -1,8 +1,11 @@
 from openai import OpenAI
 import json
+from datetime import datetime
 import inspect
 import typing
 from typing import List, Tuple, Dict, Any, Callable
+import time
+import os
 
 class InstantNeo:
     def __init__(self, api_key: str, model: str, role_setup: str,
@@ -11,18 +14,27 @@ class InstantNeo:
                  presence_penalty: float = 0.1,
                  frequency_penalty: float = 0.1,
                  skills: List[Callable[..., Any]] = None,
-                 stop=None,):
-        # Inicialización de la instancia con configuraciones y habilidades.
-        self.skills = skills if skills is not None else []
-        self.function_map = {f.__name__: f for f in self.skills}
+                 stop=None,
+                 provider=None,
+                 stream = False,
+                 log: bool = False):
+        self.api_key = api_key
         self.model = model
         self.role_setup = role_setup
         self.temperature = temperature
         self.max_tokens = max_tokens
-        self.stop = stop
         self.presence_penalty = presence_penalty
         self.frequency_penalty = frequency_penalty
-        self.instance = OpenAI(api_key=api_key)
+        self.skills = skills if skills is not None else []
+        self.function_map = {f.__name__: f for f in self.skills}
+        self.stop = stop
+        self.provider = provider
+        self.stream = stream
+        self.log = log  # Nuevo parámetro de log
+
+        if not provider:
+            self.instance = OpenAI(api_key=api_key)
+
 
     @staticmethod
     def python_type_to_string(python_type):
@@ -124,79 +136,147 @@ class InstantNeo:
                 skills.append(skill)
         return skills
 
-    def run(self,
-            prompt: str,
-            model: str = None,
-            role_setup: str = None,
-            temperature: float = None,
-            max_tokens: int = None,
-            stop=None,
-            presence_penalty: float = None,
-            frequency_penalty: float = None,
-            return_full_response: bool = False):
-        
-        # Configuración y ejecución de la solicitud al modelo.
-        
-        model = model or self.model
-        role_setup = role_setup or self.role_setup
-        temperature = temperature or self.temperature
-        max_tokens = max_tokens or self.max_tokens
-        stop = stop or self.stop
-        presence_penalty = presence_penalty or self.presence_penalty
-        frequency_penalty = frequency_penalty or self.frequency_penalty
+    def save_log(self, response, prompt, response_time):
 
-        # Configurar habilidades para la instancia
-        skills = self.set_up_skills()
-
-        # Solicitud al modelo de OpenAI. Preparar argumentos para la solicitud
-        chat_args = {
-            "model": model,
-            "messages": [
-                {"role": "system", "content": role_setup},
-                {"role": "user", "content": prompt}
-            ],
-            "temperature": temperature,
-            "max_tokens": max_tokens,
-            "stop": stop,
-            "presence_penalty": presence_penalty,
-            "frequency_penalty": frequency_penalty
+        log_entry = {
+            "timestamp": datetime.utcnow().isoformat() + "Z",
+            "prompt": prompt,
+            "response": response.to_dict(),
+            "metadata": {
+                "temperature": getattr(self, 'temperature', None),
+                "max_tokens": getattr(self, 'max_tokens', None),
+                "role_setup": getattr(self, 'role_setup', None),
+            },
+            "response_time": response_time
         }
-        # # Solicitud al modelo de OpenAI. Incluir habilidades si están disponibles
-        if skills:
-            chat_args["functions"] = skills
-            chat_args["function_call"] = "auto"
+
+        log_dir = "LOG"
+        log_file_path = os.path.join(log_dir, "responses_log.json")
+
+        # Asegurarse de que el directorio exista
+        os.makedirs(log_dir, exist_ok=True)
 
         try:
-            # Realizar la solicitud al modelo de OpenAI
-            response = self.instance.chat.completions.create(**chat_args)
-            # Retornar respuesta completa si se solicita.
-            # La respuesta completa es un JSON que incluye la respuesta y metadata
-            if return_full_response:
-                return response
-            # Procesar la respuesta para ver si incluye la llamada a una función como respuesta del modelo
-            function_call = response.choices[0].message.function_call
-            if function_call:
-                # Si el modelo responde con una llamada a una función, se extrae la información de la función
-                function_name = function_call.name
-                arguments_str = function_call.arguments
-                arguments_dic = json.loads(arguments_str)
-                #Se ejecuta directamente la función
-                is_valid_function = any(skill["name"] == function_name for skill in skills)
-                if is_valid_function:
-                    function = self.function_map.get(function_name)
-                    if function:
-                        result = function(**arguments_dic)
-                        return result
-                    else:
-                        raise ValueError(f'Función no encontrada: {function_name}')
-                else:
-                    raise ValueError(f'Función no permitida: {function_name}')
-            # Si no se llama a una función, retorna el contenido de la respuesta
-            elif response.choices[0].message.content:
-                return response.choices[0].message.content
-
+            # Leer el contenido existente del archivo
+            if os.path.exists(log_file_path):
+                with open(log_file_path, "r") as log_file:
+                    try:
+                        logs = json.load(log_file)
+                    except json.JSONDecodeError:
+                        logs = []
             else:
-                raise ValueError('No se encontró contenido ni llamada a función en la respuesta.')
+                logs = []
 
-        except Exception as e:
-            return str(e)
+            # Agregar la nueva entrada
+            logs.append(log_entry)
+
+            # Escribir todo de nuevo en el archivo
+            with open(log_file_path, "w") as log_file:
+                json.dump(logs, log_file, indent=4)
+        except IOError as e:
+            print(f"Error al escribir en el archivo de log: {e}")
+        except json.JSONDecodeError as e:
+            print(f"Error al convertir el log a JSON: {e}")
+
+    def save_error_log(self, error, context):
+        error_entry = {
+            "timestamp": datetime.utcnow().isoformat() + "Z",
+            "error": {
+                "type": type(error).__name__,
+                "message": str(error),
+            },
+            "context": context
+        }
+        with open("error_log.json", "a") as log_file:
+            log_file.write(json.dumps(error_entry, indent=4) + "\n")
+
+
+    def run(self, prompt: str, model: str = None, role_setup: str = None, 
+            temperature: float = None, max_tokens: int = None, stop=None, 
+            presence_penalty: float = None, frequency_penalty: float = None, 
+            return_full_response: bool = False, stream=False, img: str=None):
+        start_time = time.time()
+        if self.provider:
+            return self.provider.run(
+                prompt=prompt,
+                api_key=self.api_key,
+                model=model or self.model,
+                role_setup=role_setup or self.role_setup,
+                temperature=temperature or self.temperature,
+                max_tokens=max_tokens or self.max_tokens,
+                stop=stop,
+                presence_penalty=presence_penalty or self.presence_penalty,
+                frequency_penalty=frequency_penalty or self.frequency_penalty,
+                return_full_response=return_full_response,
+                stream=stream or self.stream,
+                img=img
+            )
+        else:
+            model = model or self.model
+            role_setup = role_setup or self.role_setup
+            temperature = temperature or self.temperature
+            max_tokens = max_tokens or self.max_tokens
+            stop = stop or self.stop
+            presence_penalty = presence_penalty or self.presence_penalty
+            frequency_penalty = frequency_penalty or self.frequency_penalty
+
+            skills = self.set_up_skills()
+
+            chat_args = {
+                "model": model,
+                "messages": [
+                    {"role": "system", "content": role_setup},
+                    {"role": "user", "content": prompt}
+                ],
+                "temperature": temperature,
+                "max_tokens": max_tokens,
+                "stop": stop,
+                "presence_penalty": presence_penalty,
+                "frequency_penalty": frequency_penalty
+            }
+
+            if skills:
+                chat_args["functions"] = skills
+                chat_args["function_call"] = "auto"
+
+            try:
+                response = self.instance.chat.completions.create(**chat_args)
+                response_time = time.time() - start_time
+                if return_full_response:
+                    result = response
+                else:
+                    function_call = response.choices[0].message.function_call
+                    if function_call:
+                        function_name = function_call.name
+                        arguments_str = function_call.arguments
+                        arguments_dic = json.loads(arguments_str)
+                        is_valid_function = any(skill["name"] == function_name for skill in skills)
+                        if is_valid_function:
+                            function = self.function_map.get(function_name)
+                            if function:
+                                result = function(**arguments_dic)
+                            else:
+                                raise ValueError(f'Function not found: {function_name}')
+                        else:
+                            raise ValueError(f'Function not allowed: {function_name}')
+                    elif response.choices[0].message.content:
+                        result = response.choices[0].message.content
+                    else:
+                        raise ValueError('No content or function call found in the response.')
+
+                # Guardar la respuesta en un archivo de texto si `log` está habilitado
+                if self.log:
+                    self.save_log(response,prompt,response_time)
+
+                return result
+            except Exception as e:
+                context = {
+                    "function": "run",
+                    "parameters": {
+                        "prompt": prompt,
+                        "model": model or self.model,
+                    }
+                }
+                self.save_error_log(e, context)
+                return str(e)
+
